8a23f140a9602ae95e870562f219ebc4875d4df2
Premkumar <premkumar.ashokan@talentship.io>
2022-07-20 01:14:09 +0530

    Adding index for testing optimization

--- components/last-viewed.tsx
+++ components/last-viewed.tsx
@@ -12,7 +12,7 @@ const LastViewed = ({ posts }: Props) => {
         Recently Viewed
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
-        {posts.map((post) => (
+        {posts.map((post, index) => (
           <PostPreview
             key={post.slug}
             title={post.title}
@@ -21,6 +21,7 @@ const LastViewed = ({ posts }: Props) => {
             author={post.author}
             slug={post.slug}
             excerpt={post.excerpt}
+            index={index}
           />
         ))}
       </div>
